1b48ebf567696abd2bb91cd86a112e96176069a1
cedric-joaquin <joaquin.cedric@gmail.com>
2020-11-16 17:25:28 -0800

    Done.

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module BirdWatcher
         resource '*', headers: :any, methods: [:get, :options]
       end
     end
-
+ 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
